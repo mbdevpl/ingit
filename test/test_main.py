78a1051614524632bd96edaf4598f827d2fb427b
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-02-08 18:40:08 +0900

    fixing AppVeyor

--- test/test_main.py
+++ test/test_main.py
@@ -1,15 +1,32 @@
 """Unit tests for ingit command-line interface."""
 
 import contextlib
+import logging
 import os
+import platform
 import shutil
 import tempfile
 import unittest
 
+import psutil
+
 from ingit.main import main
+
+_LOG = logging.getLogger(__name__)
 
 
 class Tests(unittest.TestCase):
+
+    def tearDown(self):
+        if platform.system() == 'Windows':
+            git_processes = [_ for _ in psutil.process_iter() if 'git' in _.name()]
+            for git_process in git_processes:
+                _LOG.warning('killing %s ...', git_process)
+                git_process.kill(git_process)
+
+        path = os.path.join(tempfile.gettempdir(), 'ingit')
+        if os.path.isdir(path):
+            shutil.rmtree(path)
 
     def test_help(self):
         with open(os.devnull, 'a') as devnull:
@@ -37,8 +54,3 @@ class Tests(unittest.TestCase):
               '--repos', 'test/examples/repos_config/example1.json', 'clone'])
         main(['--config', 'test/examples/runtime_config/example1.json',
               '--repos', 'test/examples/repos_config/example1.json', 'status'])
-
-    def tearDown(self):
-        path = os.path.join(tempfile.gettempdir(), 'ingit')
-        if os.path.isdir(path):
-            shutil.rmtree(path)
