062a303802f2c642d3503ff34212c6d11d10069f
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-02-10 00:27:59 +0900

    refactored config handling

--- ingit/runtime.py
+++ ingit/runtime.py
@@ -75,24 +75,20 @@ def run(runtime_config_path: pathlib.Path, repos_config_path: pathlib.Path,
         predicate: collections.Callable, command: str, **command_options):
     """Run the runtime."""
     runtime_config = acquire_runtime_configuration(runtime_config_path)
+    repos_path, found = resolve_runtime_config(runtime_config)
+    if not found:
+        json_to_file(runtime_config, runtime_config_path)
     repos_config = acquire_repos_configuration(repos_config_path)
+    projects = resolve_repos_config(repos_config, repos_path)
     if command == 'register':
-        register_repository(repos_config, **command_options)
+        register_repository(repos_path, repos_config, projects, **command_options)
         json_to_file(repos_config, repos_config_path)
         return
-    projects = resolve_configs(runtime_config, repos_config, predicate)
+    if predicate is not None:
+        projects = filter_projects(projects, predicate)
     for project in projects:
         implementation = getattr(project, command)
         implementation(**command_options)
-
-
-def resolve_configs(runtime_config, repos_config, predicate=None) -> t.List[Project]:
-    """Use raw JSON configuration dictionaries and a predicate to form list of projects."""
-    runtime_data = resolve_runtime_config(runtime_config)
-    projects = resolve_repos_config(repos_config, *runtime_data)
-    if predicate is None:
-        return projects
-    return filter_projects(projects, predicate)
 
 
 def resolve_runtime_config(runtime_config: dict) -> str:
@@ -113,18 +109,30 @@ def resolve_runtime_config(runtime_config: dict) -> str:
             repos_path = pathlib.Path(repos_path_str).resolve()
             break
 
-    if repos_path is None:
-        raise ValueError('no matching machine for "{}" found in configuration: """{}"""'
+    if repos_path is not None:
+        return repos_path, True
+
+    ans = ask('No matching machine for "{}" found in configuration. Add it?'.format(hostname))
+    if ans != 'y':
+        raise ValueError('No matching machine for "{}" found in configuration """{}"""'
                          .format(hostname, runtime_config))
+    repos_path = pathlib.Path('~')
+    register_machine(runtime_config, hostname, repos_path)
+    return repos_path, False
 
-    return hostname, names, repos_path
 
-
-def resolve_repos_config(repos_config, hostname, names, repos_path: pathlib.Path):
+def resolve_repos_config(repos_config: dict, repos_path: pathlib.Path):
     projects = []
     for repo in repos_config['repos']:
         name = repo['name']
-        path = repo['path'] if 'path' in repo else repos_path.joinpath(name)
+        if 'path' in repo:
+            path = pathlib.Path(repo['path'])
+            try:
+                path = path.relative_to(repos_path)
+            except ValueError:
+                pass
+        else:
+            path = repos_path.joinpath(name)
         project = Project(name=name, tags=repo['tags'], path=path, remotes=repo['remotes'])
         projects.append(project)
     return projects
@@ -136,11 +144,27 @@ def filter_projects(projects: t.Sequence[Project], predicate):
     return filtered_projects
 
 
-def register_repository(repos_config, tags: t.Sequence[str], path: pathlib.Path):
+def register_machine(runtime_config, name: str, repos_path: pathlib.Path):
+    machine_config = {'name': name, 'repos_path': str(repos_path)}
+    _LOG.warning('adding machine to configuration: %s', machine_config)
+    runtime_config['machines'].append(machine_config)
+
+
+def register_repository(repos_path: pathlib.Path, repos_config: dict, projects: t.Sequence[Project],
+                        tags: t.Sequence[str], path: pathlib.Path):
     repo = git.Repo(str(path))
     repo_data = RepoData(repo)
     _LOG.warning('registering repository: %s', repo_data)
     repo_config = repo_data.generate_repo_configuration()
-    repo_config['tags'] += tags
-    _LOG.warning('adding to configuration: %s', repo_config)
+    try:
+        repo_config['path'] = str(pathlib.Path(repo_config['path']).relative_to(repos_path))
+    except ValueError:
+        pass
+    if tags is not None:
+        repo_config['tags'] += tags
+    _LOG.warning('adding repo to configuration: %s', repo_config)
+    for project in projects:
+        if project.name == repo_config['name']:
+            raise ValueError('project named {} already exists in current configuration'
+                             .format(project.name))
     repos_config['repos'].append(repo_config)
