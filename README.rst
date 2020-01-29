4ac4073b8c992f4e739d4844a0f3bf8bffa945ed
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2020-01-29 16:31:37 +0900

    drop Python 3.5, test on 3.8

--- README.rst
+++ README.rst
@@ -477,7 +477,7 @@ Use ``--ignored`` to include ignored files in the status report, just as with
 Requirements
 ============
 
-Python version 3.5 or later.
+Python version 3.6 or later.
 
 Python libraries as specified in `<requirements.txt>`_.
 
--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import setup_boilerplate
 
 
 class Package(setup_boilerplate.Package):
-
     """Package metadata."""
 
     name = 'ingit'
@@ -21,7 +20,6 @@ class Package(setup_boilerplate.Package):
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
